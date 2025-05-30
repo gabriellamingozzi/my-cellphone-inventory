3dad31a9fb548534130f5d9c673e7c2927426a3b
gabriellamingozzi <gabriellamingozzi@gmail.com>
2025-05-30 14:43:14 -0400

    added all new components to inventory page

--- app/src/pages/inventory/inventory.tsx
+++ app/src/pages/inventory/inventory.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
 
+import Typography from '@mui/material/Typography';
+import Filters from './components/filters'
+import InventoryTable from './components/inventoryTable'
+
+const phonesInInventory = [
+  {
+    brand: "Apple",
+    model: "iPhone 15",
+    storage: "128 GB",
+    color: "Green",
+    price: 829.99
+  },
+  {
+    brand: "Apple",
+    model: "iPhone 15",
+    storage: "128 GB",
+    color: "Pink",
+    price: 829.99
+  },
+  {
+    brand: "Apple",
+    model: "iPhone 15",
+    storage: "128 GB",
+    color: "Black",
+    price: 829.99
+  },
+  {
+    brand: "Apple",
+    model: "iPhone 15",
+    storage: "128 GB",
+    color: "Blue",
+    price: 829.99
+  },
+  {
+    brand: "Apple",
+    model: "iPhone 15",
+    storage: "128 GB",
+    color: "Yellow",
+    price: 829.99
+  }
+];
+
+
 export default function Inventory() {
 
     return (
-        <div>
-            Hi!!
-        </div>
+
+        <>
+            <Typography variant="h1" gutterBottom>
+                Inventory
+            </Typography>
+            <Filters/>
+            <InventoryTable inventoryArray={phonesInInventory} />
+        </>
 
     )
 
