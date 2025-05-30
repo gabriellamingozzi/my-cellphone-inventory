9f824fccaeff714871bf3e0e7c57810f754291c1
gabriellamingozzi <gabriellamingozzi@gmail.com>
2025-05-30 14:41:35 -0400

    added navbar content

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -1,8 +1,77 @@
-export default function Navbar(){
+import { useContext } from 'react';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import InventoryIcon from '@mui/icons-material/Inventory';
+
+const drawerWidth = 240;
+
+export default function Navbar() {
+
 
     return (
-
-        <div></div>
+        <Box  sx={{ display: 'flex' }}>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Typography m={2} variant='subtitle1'>My Cellphone Store</Typography>
+                <Divider />
+                <List>
+                    <ListItem key={'Dashboard'} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <DashboardIcon/>
+                                </ListItemIcon>
+                                <ListItemText primary={'Dashboard'} />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem key={'Inventory'} disablePadding>
+                            <ListItemButton selected={true}>
+                                <ListItemIcon>
+                                    <InventoryIcon/>
+                                </ListItemIcon>
+                                <ListItemText primary={'Inventory'} />
+                            </ListItemButton>
+                        </ListItem>
+                   
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </Box>
     )
-    
+
 }
